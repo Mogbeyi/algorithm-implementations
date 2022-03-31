ecd63d52cf77eba31237d1549da31e83124ede83
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2022-03-31 13:03:12 +0100

    Check if values exist using dfs

--- data_structures/binary-tree/includes.py
+++ data_structures/binary-tree/includes.py
@@ -1,24 +1,31 @@
 from collections import deque
 from binary_tree import get_root
 
+# def includes(root, target):
+# 	queue = deque()
+# 	queue.append(root)
+
+# 	while queue:
+# 		current = queue.popleft()
+
+# 		if current.value == target:
+# 			return True
+
+# 		if current.left: queue.append(current.left)
+# 		if current.right: queue.append(current.right)
+
+# 	return False
+
 def includes(root, target):
-	queue = deque()
-	queue.append(root)
+	if not root: return False 
+	if root.value == target: return True
 
-	while queue:
-		current = queue.popleft()
+	return includes(root.left, target) or includes(root.right, target)
 
-		if current.value == target:
-			return True
-
-		if current.left: queue.append(current.left)
-		if current.right: queue.append(current.right)
-
-	return False
 
 def main():
 	root = get_root()
-	result = includes(root, 'z')
+	result = includes(root, 'c')
 
 	print("Value exist") if result else print("Value does not exist")
 
